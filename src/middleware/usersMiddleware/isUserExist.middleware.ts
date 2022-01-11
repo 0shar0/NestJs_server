de69a5186486b9ef1a3e637cce1f1f031f436a6a
0shar0 <bodiasharko@gmail.com>
2022-01-11 18:08:23 +0200

    add test

--- src/middleware/usersMiddleware/isUserExist.middleware.ts
+++ src/middleware/usersMiddleware/isUserExist.middleware.ts
@@ -5,8 +5,8 @@ import {
 } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import {User, UserDocument} from "../../schemas/user.schema";
-import {userExist} from "../../constants/constants";
+import { User, UserDocument } from 'src/schemas/user.schema';
+import { userExist } from 'src/constants/constants';
 
 @Injectable()
 export class IsUserExistMiddleware implements NestMiddleware {
